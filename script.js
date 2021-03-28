817d8e667b73fda3ddd0500f838884dc254d8a09
utibeabasiedet <76009390+utibeabasiedet@users.noreply.github.com>
2021-03-28 07:55:58 -0700

    added initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+      document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+  });
+
+
+  document.querySelector(".hamburger-menu").addEventListener("click", ()=>{
+    document.querySelector(".container").classList.toggle("change")
+});
